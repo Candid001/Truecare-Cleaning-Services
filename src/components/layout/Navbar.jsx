583a749199778320760bcdf75954e150bcb671db
Candid001 <abdulbasitmustapha1@gmail.com>
2025-11-03 06:58:38 +0100

    First Push of TrueCare Web App

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  const linkClass = ({ isActive }) =>
+    isActive
+      ? "text-[#0066CC] font-semibold"
+      : "text-gray-700 hover:text-[#0066CC]";
+
+  return (
+    <nav className="flex justify-between items-center px-6 py-4 bg-white shadow">
+      <Link to="/" className="text-xl font-bold text-[#0066CC]">
+        TrueCare
+      </Link>
+      <div className="flex gap-6">
+        <NavLink to="/" className={linkClass}>
+          Home
+        </NavLink>
+        <NavLink to="/about" className={linkClass}>
+          About Us
+        </NavLink>
+        <NavLink to="/services" className={linkClass}>
+          Our Services
+        </NavLink>
+        <NavLink to="/servicearea" className={linkClass}>
+          Service Area
+        </NavLink>
+        <NavLink to="/contact" className={linkClass}>
+          Contact Us
+        </NavLink>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../components/layout/Navbar";
+import Home from "../pages/Home";
+import AboutUs from "../pages/AboutUs";
+import ContactUs from "../pages/ContactUs";
+import Services from "../pages/Services";
+import ServiceArea from "../pages/ServiceArea";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div className="p-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/servicearea" element={<ServiceArea />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
