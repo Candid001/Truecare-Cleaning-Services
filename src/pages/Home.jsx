edc95c3e9db5f8bca24a435c2ec18a9a39dcffa6
Candid001 <abdulbasitmustapha1@gmail.com>
2025-11-07 19:09:30 +0100

    How It Works Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/layout/Hero";
 import WhoWeAre from "../components/layout/WhoWeAre";
 import WhatWeOffer from "../components/layout/WhatWeOffer";
+import HowItWorks from "../components/layout/HowItWorks";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <WhoWeAre />
       <WhatWeOffer />
+      <HowItWorks />
     </>
   );
 };
