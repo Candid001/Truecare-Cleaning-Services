377480285b13d7319afb69ea1c6682adf156cc0d
Candid001 <abdulbasitmustapha1@gmail.com>
2025-11-07 16:23:11 +0100

    WhatWeOffer Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/layout/Hero";
 import WhoWeAre from "../components/layout/WhoWeAre";
+import WhatWeOffer from "../components/layout/WhatWeOffer";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <WhoWeAre />
+      <WhatWeOffer />
     </>
   );
 };
