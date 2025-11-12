b2860e627536572159f6e9562cfe5f04302e373b
Candid001 <abdulbasitmustapha1@gmail.com>
2025-11-12 21:13:38 +0100

    Update Hero Bg

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import ServicesHero from "@components/layout/services/ServicesHero.jsx";
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
-  )
-}
+    <div>
+      <ServicesHero
+        nav={"Contact Us"}
+        heading={"We’re Here to Help"}
+        desc={`Have questions or ready to book a service? Reach out to our friendly team  we’ll respond quickly and help you get the clean you deserve.`}
+        maxWH={"lg:max-w-3/5 md:max-w-4/5"}
+        maxWD={"lg:max-w-[55%] md:max-w-4/5"}
+      />
+    </div>
+  );
+};
 
-export default ContactUs
+export default ContactUs;
