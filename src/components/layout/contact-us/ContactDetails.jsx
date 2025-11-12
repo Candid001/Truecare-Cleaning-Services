9f5c9a65f99a12153c9f453b48e92270ee0fc762
Akande Olalekan Toheeb <akandeolalekantoheeb94@gmail.com>
2025-11-12 17:41:02 +0100

    contact-us

---
+++ src/components/layout/contact-us/ContactDetails.jsx
@@ -0,0 +1,9 @@
+function ContactDetails() {
+    return(
+        <div className={`py-20 `}>
+
+        </div>
+    )
+}
+
+export default ContactDetails;
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import ServicesHero from "@/components/layout/services/ServicesHero.jsx";
 
 const ContactUs = () => {
   return (
-    <div>ContactUs</div>
+    <>
+        <ServicesHero
+            nav={'Contact Us'}
+            heading={`We're Here to Help`}
+            desc={`Have questions or ready to book a service? Reach out to our friendly team,
+             we’ll respond quickly and help you get the clean you deserve.`}
+            maxWH={'lg:max-w-1/2 md:max-w-3/5'}
+            maxWD={'lg:max-w-[55%] md:max-w-[70%]'}
+        />
+    </>
   )
 }
 
