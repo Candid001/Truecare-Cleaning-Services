3301832c71f01c8ff1f10ec6eed2843acc4d16a0
Jester-7s <Kamalolajide@gmail.com>
2026-06-28 17:15:06 +0000

    Move video section to top of homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { lazy, Suspense } from "react";
+import VideoSection from "../components/layout/landing-page/VideoSection";
 import Hero from "../components/layout/landing-page/Hero";
 import WhoWeAre from "../components/layout/landing-page/WhoWeAre";
 
@@ -7,20 +8,19 @@ const HowItWorks = lazy(() => import("../components/layout/landing-page/HowItWor
 const WhyChooseUs = lazy(() => import("../components/layout/landing-page/WhyChooseUs"));
 const WhatClientsSay = lazy(() => import("../components/layout/landing-page/WhatClientsSay"));
 const WhereWeServe = lazy(() => import("../components/layout/landing-page/WhereWeServe"));
-const VideoSection = lazy(() => import("../components/layout/landing-page/VideoSection"));
 const PriceCalculator = lazy(() => import("../components/layout/landing-page/PriceCalculator"));
 const CallToAction = lazy(() => import("../components/layout/landing-page/CallToAction"));
 
 const Home = () => {
   return (
     <>
+      <VideoSection />
       <Hero />
       <WhoWeAre />
       <Suspense fallback={null}>
         <WhatWeOffer />
         <HowItWorks />
         <WhyChooseUs />
-        <VideoSection />
         <WhatClientsSay />
         <WhereWeServe />
         <PriceCalculator />
